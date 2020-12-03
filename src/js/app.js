18643c6ede0324aea7e8fcaffe93483c16e4a091
Tolu Lawson <lawsontolu1@gmail.com>
2020-12-03 22:30:15 +0100

    -- Checkpoint

--- src/js/app.js
+++ src/js/app.js
@@ -2,13 +2,17 @@ const { GITHUB_TOKEN } = require('./api');
 
 const model = {
   data: null,
-  root: document.getElementById('root'),
   getRepos() {
     if (!this.data) {
       return null;
     }
     return this.data.user.repositories.edges.map((item) => item.node);
   },
+
+  getRepoCount() {
+    return this.data.user.repositories.totalCount;
+  },
+
   getUser() {
     if (!this.data) {
       return null;
@@ -30,6 +34,7 @@ const model = {
       emojiHTML,
     };
   },
+
   init() {
     const query = `query MyQuery {
       user(login: "tolulawson") {
@@ -53,14 +58,17 @@ const model = {
                   }
                 }
               }
+              description
               stargazerCount
               forks {
                 totalCount
               }
               updatedAt
               id
+              url
             }
           }
+          totalCount
         }
       }
     }
@@ -86,39 +94,68 @@ const model = {
 
 const userView = {
   element: document.createElement('div'),
+  root: document.getElementById('user'),
   render() {
     const userData = model.getUser();
     this.element.innerHTML = `
-      <img src='${userData.avatarUrl}' alt='Profile picture' class='avatar'/>
-      ${userData.emojiHTML}
-      <div class='name'>${userData.name}</div>
-      <div class='login'>${userData.login}</div>
+      <div class='avatar' id='avatar'>
+        <a href='#'>
+          <img src='${userData.avatarUrl}' alt='Profile picture' class='avatar-img' height=260 width=260/>
+        </a>
+        ${userData.emojiHTML}
+      </div>
+      <div id='user-top'></div>
+      <div class='user-info' id='user-info'>
+        <div class='name' id='name'>${userData.name}</div>
+        <div class='login' id='login'>${userData.login}</div>
+      </div>
       <div class='bio'>${userData.bio}</div>
     `;
-    model.root.append(this.element);
+    this.root.append(this.element);
     document.getElementById('status-image').src = userData.avatarUrl;
+    document.querySelector('.tab-nav-img').src = userData.avatarUrl;
+
+    // -- sticky avatar
+    const observer = new IntersectionObserver((entries) => {
+      if (entries[0].intersectionRatio === 0) {
+        document.querySelector('.tab-nav').classList.remove('sticky');
+        document.querySelector('.user-info').classList.add('sticky');
+        document.querySelector('.avatar').classList.add('sticky');
+      } else if (entries[0].intersectionRatio === 1) {
+        document.querySelector('.tab-nav').classList.add('sticky');
+        document.querySelector('.user-info').classList.remove('sticky');
+        document.querySelector('.avatar').classList.remove('sticky');
+      }
+    }, {
+      threshold: [0, 1],
+    });
+    observer.observe(document.querySelector('#user-top'));
   },
 };
 
 const reposView = {
-  element: document.createElement('div'),
+  element: document.createElement('ul'),
+  root: document.getElementById('repos'),
   render() {
     const repos = model.getRepos();
     repos.forEach((repo) => {
-      const singleRepo = document.createElement('div');
+      const singleRepo = document.createElement('li');
       singleRepo.innerHTML = `
-        <div>${repo.name}</div>
-        <div>
-          <span class='language' style='background-color:  ${repo.languages.edges[0].node.color};'></span>
-          <span>${repo.languages.edges[0].node.name}</span>
-          <span>${repo.stargazerCount}</span>
-          <span>${repo.forks.totalCount}</span>
-          <span>Updated at ${new Date(repo.updatedAt).toLocaleDateString('en-GB', { day: 'numeric', month: 'short' })}</span>
-        </div>
+        <div class='repo-name'>
+          <a href='${repo.url}'>${repo.name}</a>
+        <div/>
+        <div class='description'>${repo.description}</div>
+
+        <span class='language' style='background-color:  ${repo.languages.edges[0].node.color};'></span>
+        <span>${repo.languages.edges[0].node.name}</span>
+        <span>${repo.stargazerCount}</span>
+        <span>${repo.forks.totalCount}</span>
+        <span>Updated at ${new Date(repo.updatedAt).toLocaleDateString('en-GB', { day: 'numeric', month: 'short' })}</span>
       `;
       this.element.append(singleRepo);
     });
-    model.root.append(this.element);
+    this.root.append(this.element);
+    document.querySelector('.counter').textContent = model.getRepoCount();
   },
 };
 
@@ -135,14 +172,3 @@ const controller = {
 window.addEventListener('load', () => {
   controller.init();
 });
-
-const observer = new IntersectionObserver((entries) => {
-  // no intersection
-  if (entries[0].intersectionRatio === 0) {
-    document.querySelector('#tab-nav').classList.add('sticky');
-  } else if (entries[0].intersectionRatio === 1) document.querySelector('#tab-nav').classList.remove('sticky');
-}, {
-  threshold: [0, 1],
-});
-
-observer.observe(document.querySelector('#tab-nav-top'));
